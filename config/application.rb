8516288b396339cfb1f525b26f5ffb20d1c26308
keegankb <keegankwirant@rocketreferrals.com>
2025-02-13 16:54:15 -0600

    add tailwindcss

--- config/application.rb
+++ config/application.rb
@@ -15,6 +15,7 @@ module CoffeeTime
     # not contain `.rb` files, or that should not be reloaded or eager loaded.
     # Common ones are `templates`, `generators`, or `middleware`, for example.
     config.autoload_lib(ignore: %w[assets tasks])
+    config.assets.excluded_paths = Rails.root.join("app/assets/stylesheets")
 
     # Configuration for the application, engines, and railties goes here.
     #
